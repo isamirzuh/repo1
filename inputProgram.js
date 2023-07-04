027f4ae4cca3e22fc56298f1185d1492a37a5cce
Mohammad Isa Mirza <isa@Mohammads-MacBook-Air.local>
2023-07-05 00:29:53 +0530

    first commit

---
+++ inputProgram.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+
+var fname = prompt("Enter you first name: ");
+var lname = prompt("Enter you last name: ");
+
+console.log("Your Full Name is",fname,lname);
